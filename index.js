feca46bd2e4380ecb1a4d5a26b818645c8be1b46
GrzegorzNecka <strzypior@gmail.com>
2021-02-16 21:50:14 +0100

    1 file modified

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ Vue.component("route-header", {
   methods: {
     setActiveClass: function(siteNr) {
       const id = parseInt(this.$route.params.id);
-  
+
       if (id === siteNr) {
         return true;
       } else {
@@ -56,7 +56,7 @@ Vue.component("route-header", {
 
 const Member = {
   computed: {
-    contact() {
+    setMemberData() {
       const id = parseInt(this.$route.params.id);
       const newMembers = members.filter(function(member) {
         return member.id === id;
@@ -68,8 +68,8 @@ const Member = {
   template: `
 				<div class="sm:w-1/4 p-2">
 					<div class="bg-white px-6 py-8 rounded-lg shadow-lg text-center">
-						<h2 class="text-xl font-medium text-gray-700">{{ contact.first_name}}</h2>
-						<span class="text-blue-500 block mb-5">{{ contact.role}}</span>
+						<h2 class="text-xl font-medium text-gray-700">{{setMemberData.first_name}}</h2>
+						<span class="text-blue-500 block mb-5">{{setMemberData.role}}</span>
 
 						<button href="#" class="px-4 py-2 bg-blue-500 text-white rounded-full"
               >Say hello</button>
